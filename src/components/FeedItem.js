3d413fe60ed6e46cc7b478834577f2fa5cb88db1
Dm <47198076+myoneflag@users.noreply.github.com>
2022-03-15 19:17:42 +0200

    Updated features

--- src/components/FeedItem.js
+++ src/components/FeedItem.js
@@ -2,63 +2,115 @@ import React, { useEffect, useRef, useState } from 'react'
 import ReactPlayer from 'react-player'
 
 const FeedItem = ({ data, before, after, goto }) => {
-  const { video, buttons } = data // block_key, name, 
+  const { block_key, video, buttons, name, logo, link } = data
   const player = useRef(null)
+  const [loaded, setLoaded] = useState(false)
   const [playing, setPlaying] = useState(false)
   const [muted, setMuted] = useState(false)
 
+  const [width, setWidth] = useState('100%')
+  const [height, setHeight] = useState('100vh')
+
   useEffect(() => {
-    try {
-      player.current.seekTo(0)
-    } catch (err) {
-      console.log(err)
+    if (loaded) {
+      const video = document.querySelector(`#video-${block_key} video`)
+      if (
+        window.innerHeight / window.innerWidth >
+        video.videoHeight / video.videoWidth
+      ) {
+        setWidth(
+          Math.ceil(
+            (window.innerHeight /
+              window.innerWidth /
+              (video.videoHeight / video.videoWidth)) *
+              100,
+          ) + '%',
+        )
+      } else {
+        setHeight(
+          Math.ceil(
+            (video.videoHeight /
+              video.videoWidth /
+              (window.innerHeight / window.innerWidth)) *
+              100,
+          ) + 'vh',
+        )
+      }
     }
+    // eslint-disable-next-line
+  }, [loaded])
+
+  useEffect(() => {
+    if (loaded) {
+      try {
+        player.current.seekTo(0)
+      } catch (err) {
+        console.log(err)
+      }
+    }
+    // eslint-disable-next-line
   }, [before])
 
   useEffect(() => {
     setPlaying(after)
     setMuted(after)
+    // setTimeout(() => setMuted(false), 1)
+    // eslint-disable-next-line
   }, [after])
 
-  // const state = {
-  //   pip: false,
-  //   playing: true,
-  //   controls: false,
-  //   light: false,
-  //   volume: 0.8,
-  //   muted: false,
-  //   played: 0,
-  //   loaded: 0,
-  //   duration: 0,
-  //   playbackRate: 1.0,
-  //   loop: false,
-  // }
-
-  return (
+  return (video ? 
     <div className="feed-item">
-      <div className="video-section">
+      <div
+        className="video-section"
+        onClick={() => setPlaying(!playing)}
+        style={{ width, height }}
+      >
         <ReactPlayer
+          id={`video-${block_key}`}
           ref={player}
           url={`/videos/${video}`}
           width="100%"
           height="100%"
           className="video-bg"
+          onReady={() => setLoaded(true)}
           playing={playing}
           muted={muted}
+          loop={true}
+          volume={1}
         />
       </div>
-      <div className="action-section">
+      <div className="top-section">
+        <img
+          src={`/images/${logo}`}
+          className="logo"
+          width={60}
+          height={60}
+          alt="logo"
+        />
+        <h1 className="title">{name}</h1>
+        <img
+          onClick={() => setMuted(!muted)}
+          src={`/images/${muted ? 'mute' : 'unmute'}.svg`}
+          className="mute"
+          width={muted ? 35 : 50}
+          height={muted ? 35 : 50}
+          alt="mute-unmute"
+        />
+      </div>
+      <div className="bottom-section">
         {buttons.map((button, index) => (
           <button
             type="button"
             key={index}
-            className="feed-button"
+            className={`feed-button ${button.wide && 'wide'}`}
             onClick={() => goto(button.goto)}
           >
             {button.label}
           </button>
         ))}
       </div>
+    </div> : <div className="feed-item end">
+      <a href={link} className="end-link">{name}</a>
     </div>
   )
 }
